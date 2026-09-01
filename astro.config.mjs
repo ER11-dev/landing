6b11deee8b3716592be713d8188d53260ebdefd1
George Rubaylo <georgerubaylo@gmail.com>
2026-09-01 23:11:54 +0300

    chore(config): enable vercel adapter with ISR

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
 import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel";
 
 export default defineConfig({
   site: "https://upwork-orpin.vercel.app",
   output: "static",
   publicDir: "./assets",
+  adapter: vercel({
+    isr: {
+      expiration: 300,
+    },
+  }),
 });
